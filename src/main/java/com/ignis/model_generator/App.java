525f996cd26e0820d93ca9cdba0f4a424c744578
unknown <s.murali.mohan@gmail.com>
2019-08-05 07:57:21 +0530

    first commit

---
+++ src/main/java/com/ignis/model_generator/App.java
@@ -0,0 +1,13 @@
+package com.ignis.model_generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
